35f06092796cce72e36bf6454293f1fedf32f9fd
ming307- <94124200+Ming307mm@users.noreply.github.com>
2023-07-20 14:04:05 +0700

    update profile + change password

--- app/src/main/java/com/prm392team/shopf/RoomDB/UserDAO.java
+++ app/src/main/java/com/prm392team/shopf/RoomDB/UserDAO.java
@@ -26,4 +26,10 @@ public interface UserDAO {
 
     @Query("SELECT * FROM user WHERE username = :username AND password = :password")
     User getAccount(String username, String password);
+
+    @Query("UPDATE  user SET email = :email, phone = :phone, dob = :dob WHERE userId = :id")
+    void updateUser(int id, String email, String phone, String dob);
+
+    @Query("UPDATE  user SET password = :password WHERE userId = :id")
+    void changePw(String password, int id);
 }
